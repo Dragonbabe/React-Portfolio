cb6a13309583b2c2dbcef039d7e9eada029e257f
Esther Walker <equestriennesrule@yahoo.com>
2020-04-13 15:15:27 -0700

    added a header component and some css

---
+++ portfolio-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/header.css';
+
+class Header extends React.Component {
+    
+    render()  {
+        return (
+        <h1 className="jumbotron">Welcome to My Portfolio!</h1>
+        )};
+  }
+
+export default Header;
