c5cda5aa4eab71cdc2115fcffd47fd22ab702bde
Esther Walker <equestriennesrule@yahoo.com>
2020-04-17 12:48:21 -0700

    updated my contact page

--- portfolio-app/src/pages/Contact.js
+++ portfolio-app/src/pages/Contact.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Wrapper from '../components/Wrapper';
+import contacts from '../contacts.json';
 
 function Contact() {
     return (
     <Wrapper>
-        <h1>Contact me!</h1>
-        <h2>Email</h2>
-        <h3>Github</h3>
-        <h4>Linked in</h4>
+        <div className="row">
+            {contacts.map((contact, index) => 
+            <div className="col-md-6" key={index}>
+                <div className="card" style={{width: "100%"}}>
+                    <h1>{contact.name}</h1>
+                    <div className="img-container">
+                        <img alt="beep beep" src={contact.image}
+                        className="img-fluid"
+                        style={{width: "100%"}}/>
+                    </div>
+                    <div className="content">
+                        <p>
+                            {contact.paragraph}
+                        </p>
+                        <a href={contact.link}>{contact.message}</a>
+                    </div>
+                </div>
+            </div>
+            )}
+        </div>
     </Wrapper>
-    )
+    );
 };
 
 export default Contact;
