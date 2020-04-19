396c38ad486635355b6aceef40322823f35a2484
Esther Walker <equestriennesrule@yahoo.com>
2020-04-18 23:19:10 -0700

    Updates

---
+++ precache-manifest.887496731f8e7080f422a46e0b274680.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aca732e6f0e76c87969271cb388d69",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9728f86f55c9ec59a39b",
+    "url": "/React-Portfolio/static/css/main.032e8e82.chunk.css"
+  },
+  {
+    "revision": "899dc361ff53e6833a2f",
+    "url": "/React-Portfolio/static/js/2.7f275926.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.7f275926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9728f86f55c9ec59a39b",
+    "url": "/React-Portfolio/static/js/main.313ed346.chunk.js"
+  },
+  {
+    "revision": "fda50521c4e937f829df",
+    "url": "/React-Portfolio/static/js/runtime-main.877afae9.js"
+  },
+  {
+    "revision": "b8004e3a225c36ad8fc28affd6e974d1",
+    "url": "/React-Portfolio/static/media/Gigi.my.love (2).b8004e3a.jpg"
+  },
+  {
+    "revision": "78d63c0e0da13f3d64edbdfbbec56a08",
+    "url": "/React-Portfolio/static/media/emp.dir.78d63c0e.png"
+  },
+  {
+    "revision": "b98cff5260c92212ac506f3c380d1cde",
+    "url": "/React-Portfolio/static/media/fitness.dash.b98cff52.png"
+  },
+  {
+    "revision": "f7a33be3a6fc3f31acb8e817f87ea2cb",
+    "url": "/React-Portfolio/static/media/jay.snip.f7a33be3.png"
+  },
+  {
+    "revision": "75af29df3ada7c61338751fbfc84786b",
+    "url": "/React-Portfolio/static/media/mm.snip.75af29df.png"
+  },
+  {
+    "revision": "e84436883815c147fec85280efb1c922",
+    "url": "/React-Portfolio/static/media/recipe.box.e8443688.png"
+  },
+  {
+    "revision": "e6b6c0b520f4760d7ea4a2067d2a8cfc",
+    "url": "/React-Portfolio/static/media/weather.dashboard.e6b6c0b5.png"
+  }
+]);
